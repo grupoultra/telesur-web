21c8c2579ba894ba9fe0e7d529d91d4e147607ab
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-21 17:20:43 -0400

    Generando la aplicacion con Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name telesurApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the telesurApp
+ */
+angular.module('telesurApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
