d9285fd5f84fdf3fc367d9fccc8136f1fa085799
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-08-11 14:27:42 -0400

    anadiendo clase al body on nav scroll para desplazarlo un poco

--- app/scripts/directives/stickyNav.js
+++ app/scripts/directives/stickyNav.js
@@ -6,9 +6,14 @@ angular.module('telesurApp')
       angular.element($window).bind("scroll", function() {
         if (this.pageYOffset >= 100) {
           element.addClass('fixed');
+
+          angular.element( document.querySelector( 'body' ) ).addClass('fixed');
           // console.log('Scrolled below header.');
         } else {
           element.removeClass('fixed');
+
+
+          angular.element( document.querySelector( 'body' ) ).removeClass('fixed')
           // console.log('Header is in view.');
         }
       });
