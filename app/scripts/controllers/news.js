31a2fd8b6a55110fec4fd87a2332668d1b18cc0b
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-08-11 14:47:40 -0400

    Anadiendo controlador y vistar para 'news'

---
+++ app/scripts/controllers/news.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name telesurApp.controller:NewsCtrl
+ * @description
+ * # NewsCtrl
+ * Controller of the telesurApp
+ */
+angular.module('telesurApp')
+  .controller('NewsCtrl', ['$scope', 'activeDomain', function ($scope, activeDomain) {
+
+  }]);
