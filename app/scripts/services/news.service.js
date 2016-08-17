dd7be92e16325cb9ccdb494819988a5eb1ea1eea
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-08-17 16:10:19 -0400

    Refactoring:

--- app/scripts/services/news.service.js
+++ app/scripts/services/news.service.js
@@ -7,50 +7,29 @@ angular.module('telesurApp')
     var interestNews = [];
     var active = false;
 
+    var getItem = function(id, longContent){
+      var content = longContent ? ipsumService.paragraphs(6) : ipsumService.words(80);
+      return {
+        id: id,
+        active: active,
+        heroImage: 'http://placehold.it/934x314',
+        subTitle: ipsumService.sentences(1),
+        title: ipsumService.words(8),
+        content: content,
+        views: Math.floor((Math.random() * 10000) + 1),
+        link: '/#/news/' + id
+      };
+    };
+
     for( var i = 0; i < 6 ; i++){
       active = i === 0 ? true : false;
 
-      topNews.push({
-        id: i,
-        active: active,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: ipsumService.sentences(1),
-        title: ipsumService.words(8),
-        content: ipsumService.words(80),
-        views: Math.floor((Math.random() * 10000) + 1),
-        link: '/#/news/' + i
-      })
-    }
-    for( var i = 0; i < 6 ; i++){
-      relatedNews.push({
-        id: i,
-        active: active,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: ipsumService.sentences(1),
-        title: ipsumService.words(8),
-        content: ipsumService.paragraphs(6),
-        views: Math.floor((Math.random() * 10000) + 1),
-        link: '/#/news/' + i
-      })
-      interestNews.push({
-        id: i,
-        active: active,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: ipsumService.sentences(1),
-        title: ipsumService.words(8),
-        content: ipsumService.paragraphs(6),
-        views: Math.floor((Math.random() * 10000) + 1),
-        link: '/#/news/' + i
-      })
+      topNews.push(getItem(i, false));
+      relatedNews.push(getItem(i, true));
+      interestNews.push(getItem(i, true));
     }
 
-    var onFocusNews = {
-      heroImage: 'http://placehold.it/934x314',
-      subTitle: ipsumService.sentences(1),
-      title: ipsumService.words(8),
-      content: ipsumService.paragraphs(6),
-      views: Math.floor((Math.random() * 10000) + 1),
-    };
+    var onFocusNews = getItem(0, true);
 
     return {
       topNews: topNews,
