44a6d89ca58a3c56f3713cfb8d18044eac02a4b0
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-08-17 15:36:21 -0400

    Adding news service to news View

--- app/scripts/controllers/news.controller.js
+++ app/scripts/controllers/news.controller.js
@@ -8,134 +8,14 @@
  * Controller of the telesurApp
  */
 angular.module('telesurApp')
-  .controller('NewsCtrl', ['$scope', function ($scope) {
+  .controller('NewsCtrl', ['$scope', 'newsService', function ($scope, newsService) {
 
     angular.element( document.querySelector( 'body' ) ).addClass('news');
     angular.element( document.querySelector( '#logical-container' ) ).addClass('height100pc');
 
-    $scope.onFocusNews = {
-      id: 0,
-      heroImage: 'http://placehold.it/934x314',
-      subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-      title: 'Tempor anim in qui esse consectetur culpa mollit ad minim',
-      content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-      views: '11.356',
-      link: '/#/news/4'
-    };
-
-    $scope.relatedNews = [
-      {
-        id: 0,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Tempor anim in qui esse consectetur culpa mollit ad minim',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 1,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Sint labore voluptate esse cupidatat consectetur minim ut in deserunt',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 2,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Exercitation nostrud excepteur ad magna culpa quis do enim culpa',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 3,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Sit commodo elit nulla veniam incididunt consectetur veniam labore sint',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 4,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Velit excepteur anim eu aliquip cillum in non culpa mollit',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 5,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Asperiores commodi eum ex nisi numquam quam tempora. Enim eum, vel!',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      }
-    ];
-    $scope.interestNews = [
-      {
-        active: true,
-        id: 0,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Tempor anim in qui esse consectetur culpa mollit ad minim',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 1,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Sint labore voluptate esse cupidatat consectetur minim ut in deserunt',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 2,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Exercitation nostrud excepteur ad magna culpa quis do enim culpa',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 3,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Sit commodo elit nulla veniam incididunt consectetur veniam labore sint',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 4,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Velit excepteur anim eu aliquip cillum in non culpa mollit',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      },
-      {
-        id: 5,
-        heroImage: 'http://placehold.it/934x314',
-        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
-        title: 'Asperiores commodi eum ex nisi numquam quam tempora. Enim eum, vel!',
-        content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br><p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    ',
-        views: '11.356',
-        link: '/#/news/4'
-      }
-    ];
+    $scope.onFocusNews = newsService.onFocusNews;
+    $scope.relatedNews = newsService.relatedNews;
+    $scope.interestNews = newsService.interestNews;
 
     $scope.changeNews = function(id){
       $scope.onFocusNews.active = false;
--- app/scripts/services/news.service.js
+++ app/scripts/services/news.service.js
@@ -3,6 +3,8 @@
 angular.module('telesurApp')
   .factory('newsService', ['ipsumService', function(ipsumService){
     var topNews = [];
+    var relatedNews = [];
+    var interestNews = [];
     var active = false;
 
     for( var i = 0; i < 6 ; i++){
@@ -19,9 +21,42 @@ angular.module('telesurApp')
         link: '/#/news/' + i
       })
     }
+    for( var i = 0; i < 6 ; i++){
+      relatedNews.push({
+        id: i,
+        active: active,
+        heroImage: 'http://placehold.it/934x314',
+        subTitle: ipsumService.sentences(1),
+        title: ipsumService.words(8),
+        content: ipsumService.paragraphs(6),
+        views: Math.floor((Math.random() * 10000) + 1),
+        link: '/#/news/' + i
+      })
+      interestNews.push({
+        id: i,
+        active: active,
+        heroImage: 'http://placehold.it/934x314',
+        subTitle: ipsumService.sentences(1),
+        title: ipsumService.words(8),
+        content: ipsumService.paragraphs(6),
+        views: Math.floor((Math.random() * 10000) + 1),
+        link: '/#/news/' + i
+      })
+    }
+
+    var onFocusNews = {
+      heroImage: 'http://placehold.it/934x314',
+      subTitle: ipsumService.sentences(1),
+      title: ipsumService.words(8),
+      content: ipsumService.paragraphs(6),
+      views: Math.floor((Math.random() * 10000) + 1),
+    };
 
     return {
-      topNews: topNews
+      topNews: topNews,
+      interestNews: interestNews,
+      relatedNews: relatedNews,
+      onFocusNews: onFocusNews
     }
 
   }]);
