9d29a3c543b18a883f50543ed14f84c454ce2ec1
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-08-17 14:36:10 -0400

    swapping de noticias con active class en el selector de noticias en explore domain

--- app/scripts/controllers/exploreDomain.controller.js
+++ app/scripts/controllers/exploreDomain.controller.js
@@ -25,6 +25,7 @@ angular.module('telesurApp')
 
     $scope.news = [
       {
+        active: true,
         id: 0,
         heroImage: 'http://placehold.it/934x314',
         subTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id totam.',
@@ -83,9 +84,9 @@ angular.module('telesurApp')
 
     $scope.onFocusNews = $scope.news[0];
 
-    // content: '<p><b>Eu nisi occaecat commodo culpa occaecat.</b></p></br> Lorem ullamco occaecat amet nostrud nisi enim ipsum cillum veniam adipisicing consectetur non et ad. Nisi tempor eu ullamco aliqua aliquip ex ex Lorem sit. Et nulla labore excepteur non ipsum exercitation pariatur ea velit voluptate est aute esse nulla. Enim culpa sunt ex id irure ex ullamco deserunt veniam nisi esse amet cupidatat minim. Pariatur commodo sit sit aliquip nulla sunt.</p></br>    <p>Nisi ad tempor occaecat est consequat fugiat velit aliqua. In reprehenderit excepteur exercitation fugiat esse reprehenderit fugiat esse nulla. Cillum ex anim occaecat sunt mollit ea nisi ipsum adipisicing. Mollit laboris adipisicing esse magna adipisicing minim. Exercitation ex velit culpa voluptate. Ad cupidatat pariatur incididunt cupidatat consectetur tempor anim dolor adipisicing incididunt aute culpa tempor.</p></br>    <p>Amet quis adipisicing consectetur exercitation quis mollit. Deserunt reprehenderit mollit Lorem pariatur irure commodo veniam sunt nisi amet. Adipisicing officia do mollit incididunt occaecat quis sit. Incididunt sunt veniam voluptate sit minim consequat adipisicing adipisicing dolore labore nostrud aliqua deserunt. Duis laboris sit labore enim mollit ex excepteur sunt veniam commodo. Velit in elit ullamco cillum in minim aliqua veniam labore occaecat aute sint et. Mollit tempor velit occaecat ullamco et consequat amet irure.</p></br>    <p>Adipisicing fugiat ex ea consequat nostrud cupidatat excepteur commodo magna incididunt minim sint. Excepteur esse nostrud reprehenderit est. Culpa deserunt officia ullamco veniam. Nulla proident dolore enim laboris mollit quis aliqua. Mollit consequat cillum do eu dolor.</p></br>    '
-
     $scope.changeNews = function(id){
+      $scope.onFocusNews.active = false;
       $scope.onFocusNews = $scope.news[id];
+      $scope.news[id].active = true;
     };
   }]);
