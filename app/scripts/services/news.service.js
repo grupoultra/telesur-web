899fcd0255b05f9067669e48acf07257a0b661ba
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-08-17 15:25:18 -0400

    Implementando topNews en el servicio news

--- app/scripts/services/news.service.js
+++ app/scripts/services/news.service.js
@@ -1,6 +1,27 @@
 'use strict';
 
 angular.module('telesurApp')
-  .factory('news', function(){
+  .factory('newsService', ['ipsumService', function(ipsumService){
+    var topNews = [];
+    var active = false;
 
-  });
+    for( var i = 0; i < 6 ; i++){
+      active = i === 0 ? true : false;
+
+      topNews.push({
+        id: i,
+        active: active,
+        heroImage: 'http://placehold.it/934x314',
+        subTitle: ipsumService.sentences(1),
+        title: ipsumService.words(8),
+        content: ipsumService.words(80),
+        views: Math.floor((Math.random() * 10000) + 1),
+        link: '/#/news/' + i
+      })
+    }
+
+    return {
+      topNews: topNews
+    }
+
+  }]);
